12951e9e410551eeb16b5bb1d252512986bbb3db
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-30 08:16:39 -0600

    Arrays in C#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Arrays");
+
+string[] weekDays = new string[7];
+Console.WriteLine(weekDays);
+Console.WriteLine(weekDays[0]); //Imprime el contenido de esa linea
+
+weekDays[0] = ("Mondays");
+weekDays[1] = ("Tuesday");
+weekDays[2] = ("Wednesday");
+weekDays[3] = ("Thrusday");
+weekDays[4] = ("Friday");
+weekDays[5] = ("Saturday");
+weekDays[6] = ("Sunday");
+Console.WriteLine(weekDays[0]); 
+Console.WriteLine(weekDays[3]); 
+Console.WriteLine(weekDays[6]);
+
+int[] primes = new int[]{2,3,5,7,11,13};
+Console.WriteLine(primes[1]);
+
+int[] pairs = {2,4,6,8,10};
+Console.WriteLine(pairs[4]);
